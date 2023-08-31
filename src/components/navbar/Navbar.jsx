370faa1f5f744aae38daf9ed8d663e7938fad1d2
sachin <rajeshsachin786@gmail.com>
2023-09-01 02:24:19 +0530

    implement bolg about etc pages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
+import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
 // import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 // import { signOut, useSession } from "next-auth/react";
 
@@ -48,7 +49,7 @@ const Navbar = () => {
         portfolio
       </Link>
       <div className={styles.links}>
-        {/* <DarkModeToggle /> */}
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
